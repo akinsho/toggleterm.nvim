f1244711da05b89569320e9a42278a8c8e8eea27
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-04-30 18:04:19 +0100

    test: comment out new case to test CI

--- tests/terminal_spec.lua
+++ tests/terminal_spec.lua
@@ -108,7 +108,7 @@ describe("ToggleTerm tests:", function()
       assert.is_true(ui.term_has_open_win(term))
     end)
 
-    it("should close on exit", function()
+    pending("should close on exit", function()
       local term = Terminal:new():toggle()
       assert.is_true(ui.term_has_open_win(term))
       term:send("exit")
