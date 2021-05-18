36690b66d341a779f99ffbcd5ed7e5f74f353274
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-05-18 21:42:21 +0100

    refactor: tidy up dated color module

--- lua/toggleterm/colors.lua
+++ lua/toggleterm/colors.lua
@@ -6,40 +6,30 @@ local M = {}
 -----------------------------------------------------------
 -- Helpers
 -----------------------------------------------------------
+---Convert a hex color to an rgb color
+---@param color string
+---@return number
+---@return number
+---@return number
 local function to_rgb(color)
-  local r = tonumber(string.sub(color, 2, 3), 16)
-  local g = tonumber(string.sub(color, 4, 5), 16)
-  local b = tonumber(string.sub(color, 6), 16)
-  return r, g, b
+  return tonumber(color:sub(2, 3), 16), tonumber(color:sub(4, 5), 16), tonumber(color:sub(6), 16)
 end
 
 -- SOURCE: https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
 local function shade_color(color, percent)
   local r, g, b = to_rgb(color)
-
   -- If any of the colors are missing return "NONE" i.e. no highlight
   if not r or not g or not b then
     return "NONE"
   end
-
   r = math.floor(tonumber(r * (100 + percent) / 100))
   g = math.floor(tonumber(g * (100 + percent) / 100))
   b = math.floor(tonumber(b * (100 + percent) / 100))
-
-  r = r < 255 and r or 255
-  g = g < 255 and g or 255
-  b = b < 255 and b or 255
+  r, g, b = r < 255 and r or 255, g < 255 and g or 255, b < 255 and b or 255
 
   -- see: https://stackoverflow.com/questions/37796287/convert-decimal-to-hex-in-lua-4
-  r = string.format("%x", r)
-  g = string.format("%x", g)
-  b = string.format("%x", b)
-
-  local rr = string.len(r) == 1 and "0" .. r or r
-  local gg = string.len(g) == 1 and "0" .. g or g
-  local bb = string.len(b) == 1 and "0" .. b or b
-
-  return "#" .. rr .. gg .. bb
+  r, g, b = string.format("%0x", r), string.format("%0x", g), string.format("%0x", b)
+  return "#" .. r .. g .. b
 end
 
 --- Determine whether to use black or white text
@@ -57,11 +47,8 @@ function M.color_is_bright(hex)
   end
   -- Counting the perceptive luminance - human eye favors green color
   local luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255
-  if luminance > 0.5 then
-    return true -- Bright colors, black font
-  else
-    return false -- Dark colors, white font
-  end
+  -- If luminance is > 0.5 -> Bright colors, black font else Dark colors, white font
+  return luminance > 0.5
 end
 
 function M.is_bright_background()
