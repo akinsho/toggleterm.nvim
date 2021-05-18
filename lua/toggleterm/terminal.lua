00ae751b3aae87da78e299f7f7c5b4391b7715e5
Luke Kershaw <35707277+l-kershaw@users.noreply.github.com>
2021-05-18 20:48:27 +0100

    Typo fix

--- lua/toggleterm/terminal.lua
+++ lua/toggleterm/terminal.lua
@@ -9,8 +9,8 @@ local api = vim.api
 local fmt = string.format
 local fn = vim.fn
 
-local command_sep = has("win32") and "&" or ";"
-local comment_sep = has("win32") and "::" or "#"
+local command_sep = vim.fn.has("win32") and "&" or ";"
+local comment_sep = vim.fn.has("win32") and "::" or "#"
 
 ---@type Terminal[]
 local terminals = {}
