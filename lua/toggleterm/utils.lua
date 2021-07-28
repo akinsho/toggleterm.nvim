3dc3ef8b802b6423f63123b45ea8b90b3a2bf8ec
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-07-28 22:41:19 +0100

    feat(utils): add notify helper

--- lua/toggleterm/utils.lua
+++ lua/toggleterm/utils.lua
@@ -3,6 +3,7 @@ local M = {}
 local api = vim.api
 local fn = vim.fn
 local fmt = string.format
+local levels = vim.log.levels
 
 ---Print a message to vim's commandline
 ---@param msg string
@@ -10,6 +11,14 @@ local fmt = string.format
 function M.echomsg(msg, hl)
   hl = hl or "Title"
   api.nvim_echo({ { msg, hl } }, true, {})
+end
+
+---Inform a user about something
+---@param msg string
+---@param level 'error' | 'info' | 'warn'
+function M.notify(msg, level)
+  level = level and levels[level:upper()] or levels.INFO
+  vim.notify(fmt("[toggleterm]: %s", msg), level)
 end
 
 --- Source: https://teukka.tech/luanvim.html
