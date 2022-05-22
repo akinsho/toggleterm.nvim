93c2f2ca96af74f22f470e2fbcbd5e89b1faa63d
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-05-22 18:54:27 +0200

    chore(utils): remove unused autocommand workaround

--- lua/toggleterm/utils.lua
+++ lua/toggleterm/utils.lua
@@ -21,20 +21,6 @@ function M.notify(msg, level)
   vim.notify(fmt("[toggleterm]: %s", msg), level)
 end
 
---- Source: https://teukka.tech/luanvim.html
---- @param definitions table<string,table>
-function M.create_augroups(definitions)
-  for group_name, definition in pairs(definitions) do
-    vim.cmd("augroup " .. group_name)
-    vim.cmd("autocmd!")
-    for _, def in pairs(definition) do
-      local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
-      vim.cmd(command)
-    end
-    vim.cmd("augroup END")
-  end
-end
-
 ---@private
 ---Helper function to derive the current git directory path
 ---@return string|nil
