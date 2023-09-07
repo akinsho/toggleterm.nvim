0e4dcb8f0914bd191f732cae826df59f174359fe
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-09-07 08:13:16 +0100

    revert: determine custom terminal ids on spawn (#488)
    This reverts commit 8572917413dd039d1a53b007df5c571e2a3b8ad7.

--- lua/toggleterm/terminal.lua
+++ lua/toggleterm/terminal.lua
@@ -194,7 +194,7 @@ function Terminal:new(term)
   local conf = config.get()
   self.__index = self
   term.direction = term.direction or conf.direction
-  term.id = id
+  term.id = id or next_id()
   term.float_opts = vim.tbl_deep_extend("keep", term.float_opts or {}, conf.float_opts)
   term.clear_env = term.clear_env
   term.auto_scroll = vim.F.if_nil(term.auto_scroll, conf.auto_scroll)
@@ -452,7 +452,6 @@ end
 ---Spawn terminal background job in a buffer without a window
 function Terminal:spawn()
   if not self.bufnr or not api.nvim_buf_is_valid(self.bufnr) then self.bufnr = ui.create_buf() end
-  if not self.id then self.id = next_id() end
   self:__add()
   if api.nvim_get_current_buf() ~= self.bufnr then
     api.nvim_buf_call(self.bufnr, function() self:__spawn() end)
